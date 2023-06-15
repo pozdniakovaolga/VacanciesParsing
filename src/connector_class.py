f518b5ca07b112d860f9b902bae5191001e4ef19
pozdniakova olga <caramelca07@mail.ru>
2023-06-15 02:43:14 +0200

    Updated Connector class

--- src/connector_class.py
+++ src/connector_class.py
@@ -29,23 +29,25 @@ class Connector(ABC):
 class JsonConnector(Connector):
     """Абстрактный класс для работы с json-файлом вакансий """
 
+    file_name = "vacancies.json"
+
     def add_vacancies(self, vacancies) -> None:
         """Добавляет вакансии в json-файл """
-        if os.stat("vacancies.json").st_size == 0:  # если файл пустой
-            with open("vacancies.json", "w", encoding='utf8') as file:
+        if os.stat(self.file_name).st_size == 0:  # если файл пустой
+            with open(self.file_name, "w", encoding='utf8') as file:
                 json.dump(vacancies, file, indent=2, ensure_ascii=False)  # записываем данные в файл
 
         else:  # если файл не пустой
-            with open("vacancies.json", "r", encoding='utf8') as file:
+            with open(self.file_name, "r", encoding='utf8') as file:
                 data = json.load(file)  # выгружаем все данные из файла
                 data.extend(vacancies)  # добавляем новые данные
-            with open("vacancies.json", "w", encoding='utf8') as file:
+            with open(self.file_name, "w", encoding='utf8') as file:
                 json.dump(data, file, indent=2, ensure_ascii=False)  # перезаписываем файл
 
     def filter_vacancies(self, search_word) -> list:
         """Возвращает вакансии из json-файла, соответствующие поисковому запросу: search_word """
 
-        with open("vacancies.json", "r", encoding='utf8') as file:
+        with open(self.file_name, "r", encoding='utf8') as file:
             data = json.load(file)  # выгружаем данные из файла
             filtered_list = []
 
@@ -59,7 +61,7 @@ class JsonConnector(Connector):
     def top_vacancies(self, top_n) -> list:
         """Возвращает top_n вакансий из json-файла, отсортированные по уровню зарплаты """
 
-        with open("vacancies.json", "r", encoding='utf8') as file:
+        with open(self.file_name, "r", encoding='utf8') as file:
             data = json.load(file)  # выгружаем данные из файла
             data.sort(key=lambda dictionary: dictionary["salary_from"], reverse=True)  # сортируем по зарплате
 
@@ -68,5 +70,5 @@ class JsonConnector(Connector):
     def delete_vacancies(self) -> None:
         """Удаляет все вакансии из json-файла """
 
-        with open("vacancies.json", "w", encoding='utf8') as file:
+        with open(self.file_name, "w", encoding='utf8') as file:
             pass  # перезаписываем файл пустым
